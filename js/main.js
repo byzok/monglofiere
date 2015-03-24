e89dfbb3c71d2be144f9a9b2efc131f58fb89253
Julia Arapova <arapova.jul@gmail.com>
2015-03-24 21:39:36 +0300

    @media 1200px

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 
 //функю открітия меню
 function nav_toggle(){
-
-$(".nav-menu").slideToggle();
-
+  $(".nav-menu").slideToggle();
 }
-
 
 $(document).ready(function(){
 
@@ -13,59 +10,64 @@ $(document).ready(function(){
 //  открывание и закрывание меню
 if($(".sandwitch:visible").length>0){
   $(".nav-menu").removeClass("visible-md").removeClass("visible-lg").hide();
-  }
+}
 $("a.sandwitch").on("click",nav_toggle);
 
 //активация слайдера
 
+  $(".owl-my").owlCarousel({
+  items:3,
+  loop:true,
+  center:true,
+  margin:10,
+  nav:true,
+  autoplayHoverPause:true,
+  navText:["<img src='pict/big/icons/arrow-l.png'>","<img src='pict/big/icons/arrow-r.png'>"],
+  responsive:{
+    0:{items:1,nav:true},
+    992:{items:3,nav:true}
+    }
+  });
 
-
-
-
-        $('.owl-my').owlCarousel({
-        items:3,
-        loop:true,
-        center:true,
-        margin:10,
-        nav:true,
-        autoplayHoverPause:true,
-        navText:["<img src='http://placehold.it/20x20'>","<img src='http://placehold.it/20x20'>"],
-        responsive:{
-          0:{items:1,nav:true},
-          992:{items:3,nav:true}
-        }
-        });
-
-
-        $('.owl-reviews').owlCarousel({
-          
-        items:2,
-        loop:true,
-        center:true,
-        margin:100,
-        nav:true,
-        autoplayHoverPause:true,
-        navText:["<img src='http://placehold.it/20x20'>","<img src='http://placehold.it/20x20'>"],
-        responsive:{
-          0:{items:1,nav:true},
-          992:{items:2,nav:true}
-        }
-        });
-
-
-//попытки подключения счетчика
-
-//$.countdown.setDefaults({description: 'Until liftoff'});
-//$("#defaultCountdown").countdown({since: new Date(2014, 12-1, 25)});
 
 // TABS
- $('#wrapper span').click(function() {
-    if ($(this).attr('class') != $('#wrapper').attr('class') ) {
-      $('#wrapper').attr('class',$(this).attr('class'));
+ $("#wrapper span").click(function() {
+    if ($(this).attr("class") != $("#wrapper").attr("class") ) {
+      $("#wrapper").attr("class",$(this).attr("class"));
     }
-    $('#wrapper span').click( function() {
-      $(this).addClass('active').siblings().removeClass('active');
+    $("#wrapper span").click( function() {
+      $(this).addClass("active").siblings().removeClass("active");
     });
   });
-//$(".collapse").collapse()
+
+
+
+$(document).ready(function() {
+ 
+  $("#owl-reviews").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 600,
+      singleItem:true
+  });
+ 
+});
+
+
+
+
+//Timer
+  var newYear = new Date(); 
+  newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
+  $("#defaultCountdown").countdown({until: "+70d"});
+   
+  $("#removeCountdown").click(function() { 
+      var destroy = $(this).text() === "Remove"; 
+      $(this).text(destroy ? "'Re-attach" : "Remove"); 
+      $("#defaultCountdown").countdown(destroy ? "destroy" : {until: newYear}); 
+  });
+
+
+
 });

78b8f4a478f30fc779ce882d6d46617e331d1720
Arapova Julia <arapova.jul@gmail.com>
2015-04-14 21:59:17 +0300

    responsive_page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-//функю открітия меню
+//функция открытия меню
 function nav_toggle(){
   $(".nav-menu").slideToggle();
 }
@@ -7,14 +7,26 @@ function nav_toggle(){
 $(document).ready(function(){
 
 
+//Плавный скролл
+   $("a.scroll").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 800,
+         easing: "swing"
+      });
+      return false;
+   });
+
+
 //  открывание и закрывание меню
-if($(".sandwitch:visible").length>0){
-  $(".nav-menu").removeClass("visible-md").removeClass("visible-lg").hide();
-}
-$("a.sandwitch").on("click",nav_toggle);
+  if($(".sandwitch:visible").length>0){
+    $(".nav-menu").removeClass("visible-md").removeClass("visible-lg").hide();
+  }
+  $("a.sandwitch").on("click",nav_toggle);
+
 
 //активация слайдера
-
   $(".owl-my").owlCarousel({
   items:3,
   loop:true,
@@ -31,7 +43,7 @@ $("a.sandwitch").on("click",nav_toggle);
 
 
 // TABS
- $("#wrapper span").click(function() {
+  $("#wrapper span").click(function() {
     if ($(this).attr("class") != $("#wrapper").attr("class") ) {
       $("#wrapper").attr("class",$(this).attr("class"));
     }
@@ -40,7 +52,7 @@ $("a.sandwitch").on("click",nav_toggle);
     });
   });
 
-
+/*
 
 $(document).ready(function() {
  
@@ -54,20 +66,36 @@ $(document).ready(function() {
  
 });
 
+*/
 
 
+//Timer 70days
 
-//Timer
+
+/*
   var newYear = new Date(); 
   newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
   $("#defaultCountdown").countdown({until: "+70d"});
-   
+   //$('#padZeroes').countdown({until: liftoffTime, padZeroes: true});
   $("#removeCountdown").click(function() { 
       var destroy = $(this).text() === "Remove"; 
       $(this).text(destroy ? "'Re-attach" : "Remove"); 
       $("#defaultCountdown").countdown(destroy ? "destroy" : {until: newYear}); 
   });
 
+*/
 
+
+//Timer
+  var newYear = new Date(); 
+  newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
+  $('#defaultCountdown').countdown({until: newYear}); 
+
+   
+  $('#removeCountdown').click(function() { 
+      var destroy = $(this).text() === 'Remove'; 
+      $(this).text(destroy ? 'Re-attach' : 'Remove'); 
+      $('#defaultCountdown').countdown(destroy ? 'destroy' : {until: newYear}); 
+  });
 
 });
